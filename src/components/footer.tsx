c7076f118a5641f42e7e8eecf40a60d52d5f2361
rabiasohail098 <rabiasohail1209@gmail.com>
2024-12-03 04:58:22 +0500

    done

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -8,8 +8,8 @@ import { FaInstagram } from "react-icons/fa";
 const Footer = () => {
   return (
     <div className='flex md:w-full sm:w-full w-full h-[60px] pt-4 justify-around mt-4 bg-slate-900'>
-        <h1 className='text-white text-lg flex '>Next.JS Version &nbsp;<FaRegCopyright className='mt-1 text-[22px]'/>&nbsp; made by Rabia Sohail</h1>
-        <div className='flex gap-3 text-white text-xl'>
+        <h1 className='text-white md:text-lg text-sm flex '>Next.JS Version &nbsp;<FaRegCopyright className='mt-1 text-[22px]'/>&nbsp; made by Rabia Sohail</h1>
+        <div className=' md:flex gap-3 text-white md:text-xl sm:text-sm hidden'>
 <Link href="https://www.facebook.com/parniyasohail098"><FaFacebook /></Link>
 <Link href="https://www.linkedin.com/in/rabia-sohail-684740278/"><FaLinkedin /></Link>
 <Link href="https://www.youtube.com/@Parniya098"><CiYoutube className='text-2xl' /></Link>
